d0880d54317875e4952d3f22f893e08b88ced4a7
Pathomphong Intajai <pathomphong.i@codium.co>
2020-08-05 14:02:31 +0700

    re

--- 1.correctness.ts
+++ 1.correctness.ts
@@ -12,3 +12,8 @@ Questions:
 1) How to ensure that the input is a string at compile time?
 2) What did you learn from this exercise?
 */
+
+function scriptAt(s: string) {
+    return s.indexOf("Script");
+}
+ console.log(scriptAt("sfasfafasfasfsfsfScriptsfalfalfa"))
